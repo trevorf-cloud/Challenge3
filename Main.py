fa14f4036d7f3f729ca7b6a9c72f72c9f06fcbcb
Trevor Freyder <freydertrevor@gmail.com>
2026-04-17 22:33:31 -0600

    Wifi section done Please enter the commit message for your changes. Lines starting

---
+++ Main.py
@@ -0,0 +1,30 @@
+import network
+import time
+
+# Wi-Fi credentials
+SSID = "607 University Ave"
+PASSWORD = "Bluetruck629"
+
+# Create Wi-Fi interface and turn it on
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+print("Connecting to Wi-Fi...")
+
+# Try to connect if not already connected
+if not wlan.isconnected():
+    wlan.connect(SSID, PASSWORD)
+
+    # Wait until connection is successful
+    while not wlan.isconnected():
+        time.sleep(1)
+        print("Connecting...")
+
+# Check result
+if wlan.isconnected():
+    print("Connected successfully")
+    print("IP address:", wlan.ifconfig()[0])  # device network address
+else:
+    print("Connection failed")
+
+print("Done")
